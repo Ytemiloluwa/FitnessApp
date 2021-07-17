e55e8dc9e30203e74f8c6622cfd6b63b85aebcbc
temi <ytemiloluwa@yahoo.com>
2021-07-17 12:20:53 +0100

    Initial Commit

---
+++ Shared/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  Shared
+//
+//  Created by Temi on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
